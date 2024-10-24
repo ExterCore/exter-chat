89a8f28b80d73eb568ca2fee66d757c4580dd5d2
sobing4413 <95325881+sobing4413@users.noreply.github.com>
2024-10-24 10:20:34 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+author "sobing"
+
+description "EXTER-CHAT NOPIXEL DESIGN 4.0"
+
+version "1.0"
+
+ui_page 'nui/dist/index.html'
+
+client_scripts {
+  "client/*.lua"
+}
+
+server_scripts {
+  "server/*.lua",
+  "server/sv_open.lua"
+}
+
+shared_script {
+  "shared/*.lua"
+}
+
+files {
+    "web/dist/**/*",
+}
